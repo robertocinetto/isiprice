7f7b2160f51181949cfec94af9e6f5a7d44aae41
Roberto Cinetto <roberto.cinetto@gmail.com>
2014-10-15 22:38:15 +0200

    rearrage of professionals db fields in migration

--- db/migrate/20140728095959_create_professionals.rb
+++ db/migrate/20140728095959_create_professionals.rb
@@ -6,9 +6,17 @@ class CreateProfessionals < ActiveRecord::Migration
       t.string :email
       t.string :profile_img
       t.string :profession
+      t.string :service_type
+      t.integer :hour_cost
       t.string :address
+      t.integer :cap
+      t.string :city
+      t.string :county
+      t.string :country
       t.float  :latitude
       t.float  :longitude
+      t.integer :cell
+      t.string :logo
 
       t.timestamps
     end
